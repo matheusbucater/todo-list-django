822ff870f92d8ed365f67ed081e7094bf5889b62
matheusbucater <matheusp.bucater@gmail.com>
2021-09-05 07:23:21 -0300

    added some css

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,6 +62,11 @@ INTERNAL_IPS = [
 ]
 
 ROOT_URLCONF = 'todo_list.urls'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 TEMPLATES = [
     {
